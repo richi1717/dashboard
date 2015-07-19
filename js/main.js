bc429eaf8ba5f19ee7c960b7543472a1a780a41b
Richi Jeffery <kikki_sawn@yahoo.com>
2015-07-19 14:47:04 -0700

    add dashboard to rep

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+    $('.user-signup-chart').highcharts({
+        chart: {
+            type: 'areaspline'
+        },
+        title: {
+            text: 'Recent Users'
+        },
+        yAxis: {
+            
+            title: {
+                text: 'Signups Per Day'
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' units'
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series:
+        [{
+            name: 'Users',
+            data: [3, 4, 3, 5, 4, 10, 12]
+        }]
+    });
+});
